8fdb49a569da4721dcf777c068f152ddf323fb29
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-04 15:22:47 +0300

    chore: обновляет примеры

--- example/index.js
+++ example/index.js
@@ -1,8 +1,27 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-new-object */
 /* eslint-disable no-unused-vars */
 
 let _ = require("lodash");
 
-// Ножно закомментить некст строку
-_ = { map: () => {} };
+const func = (val, index) => {
+  // noop
+};
 
-const m = _.map([], () => {});
+// Можно закомментить некст строку
+// _ = { map: func };
+
+const testArr = [1, 2, 3];
+const testObj = {};
+const testNewArr = new Array(5);
+const testNewObj = new Object();
+
+// на Массив
+_.map([], func);
+_.map(testArr, func);
+_.map(testNewArr, func);
+
+// на Объект
+_.map({}, func);
+_.map(testObj, func);
+_.map(testNewObj, func);
